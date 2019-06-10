8687428876dc0c3d559b8cb7edf34b943b70624b
Rômulo Garofalo <romulogarofalo@gmail.com>
2019-06-10 18:45:15 -0300

    :sparkles: MOVIE - adding the route and controller os movies - feat

---
+++ src/app/Movie.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
+}
